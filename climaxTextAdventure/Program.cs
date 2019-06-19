f2fda9bbc66e7e85a158bf0d3d041e6929384266
McGuire Price <mcguire02170@gmail.com>
2019-06-19 13:46:32 -0500

    inital commit

---
+++ climaxTextAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace climaxTextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
