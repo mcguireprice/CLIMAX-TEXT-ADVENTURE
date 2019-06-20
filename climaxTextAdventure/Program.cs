52a5aeeb0bbd5e23c211fe02b59701dd26ec8a38
McGuire Price <mcguire02170@gmail.com>
2019-06-20 08:04:19 -0500

    Starteed the Davidpath, added go to him

--- climaxTextAdventure/Program.cs
+++ climaxTextAdventure/Program.cs
@@ -12,7 +12,28 @@ namespace climaxTextAdventure
             Console.WriteLine("*YOU PERFORM THE CHOREOGRAPHED DANCE*");
             Console.WriteLine("*SONG ENDS*");
             Console.WriteLine("EMMANUELLE: Good job everyone! I am so happy to see all of your hard work starting to pay off! As you know we are heading to America tommorow and I couldn't be more proud to show the American audiences what French dancing is really about! Now I want you to all rest and have a good time before we head off tomorrow, I've made punch and set out food. Also, Daddy will be the DJ for tonight so if you want some good music make sure to kiss up to him. HAVE FUN!");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Do you want to go hangout with your boyfriend David or go and hangout with Daddy the DJ? (TYPE: David or Daddy)");
+            string choice1 = Console.ReadLine();
+            if (choice1 == "David")
+            {
+                Console.WriteLine("*YOU START TO WALK OVER TO DAVID BUT SEE HE IS FLIRTING WITH SOMEONE ELSE.");
+                Console.WriteLine("---------------------------------------------------------------------------");
+                Console.WriteLine("*Do you confront him or go to your friend Lou? (Type: David or Lou)");
+                string choice11 = Console.ReadLine();
+                if (choice11 == "David") ;
+                {
 
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("*YOU WALK OVER TO DADDY THE DJ ON STAGE*");
+            }
         }
     }
     
