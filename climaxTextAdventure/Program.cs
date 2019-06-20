10a710d89f48b81e2755c93a85bfcc10944db6c0
McGuire Price <mcguire02170@gmail.com>
2019-06-20 07:46:13 -0500

    Finished Draft One of Opening

--- climaxTextAdventure/Program.cs
+++ climaxTextAdventure/Program.cs
@@ -6,7 +6,14 @@ namespace climaxTextAdventure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("SETTING- An old school that is now used by the dance crew. The gym is where they perform and have the party but the old classrooms have been turned into individual dorm rooms.");
+            Console.WriteLine("*DANCE MUSIC PLAYS IN THE BACKGROUND*");
+            Console.WriteLine("*YOU WALK OUT ON THE DANCE FLOOR WITH THE REST OF THE DANCE CREW*");
+            Console.WriteLine("*YOU PERFORM THE CHOREOGRAPHED DANCE*");
+            Console.WriteLine("*SONG ENDS*");
+            Console.WriteLine("EMMANUELLE: Good job everyone! I am so happy to see all of your hard work starting to pay off! As you know we are heading to America tommorow and I couldn't be more proud to show the American audiences what French dancing is really about! Now I want you to all rest and have a good time before we head off tomorrow, I've made punch and set out food. Also, Daddy will be the DJ for tonight so if you want some good music make sure to kiss up to him. HAVE FUN!");
+
         }
     }
+    
 }
