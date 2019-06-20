787a06c01d9fc0bb97da0d1a7b81c55219ac4277
McGuire Price <mcguire02170@gmail.com>
2019-06-20 18:04:51 -0500

    Added Dance with David path. Added go see Lou. Added talk with Daddy and Emmanuelle

--- climaxTextAdventure/Program.cs
+++ climaxTextAdventure/Program.cs
@@ -15,7 +15,7 @@ namespace climaxTextAdventure
             Console.WriteLine("*YOU WALK OUT ON THE DANCE FLOOR WITH THE REST OF THE DANCE CREW*");
             Console.WriteLine("*YOU PERFORM THE CHOREOGRAPHED DANCE*");
             Console.WriteLine("*SONG ENDS*");
-            Console.WriteLine("EMMANUELLE: Good job everyone! I am so happy to see all of your hard work starting to pay off! As you know we are heading to America tommorow and I couldn't be more proud to show the American audiences what French dancing is really about! Now I want you to all rest and have a good time before we head off tomorrow, I've made punch and set out food. Also, Daddy will be the DJ for tonight so if you want some good music make sure to kiss up to him. HAVE FUN!");
+            Console.WriteLine("EMMANUELLE: Good job everyone! I am so happy to see all of your hard work starting to pay off! As you know we are heading to America tommorow and I couldn't be more proud to show the American audiences what French dancing is really about! Now I want you to all rest and have a good time before we head off tomorrow, I've made alcoholic punch and set out food. Also, Daddy will be the DJ for tonight so if you want some good music make sure to kiss up to him. HAVE FUN!");
 
             Console.WriteLine("---------------------------------------------------------------------------");
             Console.WriteLine("Do you want to go hangout with your boyfriend David or go and hangout with Daddy the DJ? (TYPE: David or Daddy)");
@@ -40,9 +40,16 @@ namespace climaxTextAdventure
                     Console.WriteLine("Do you go dance with David or go talk to your choerographter Emmanuelle? (Type Dance or Emmanuelle)");
                     string choice12 = Console.ReadLine();
 
-                    if (choice12 == "Dance")//Dance with David
+                    if (choice12 == "Dance" || choice12 == "dance")//Dance with David
                     {
+                        Console.WriteLine("YOU: Fine. But don't expect me to hae any fun.");
+                        Console.WriteLine("DAVID: Don't worry, I won't have any either, but first lets get something to drink or eat!");
+                        Console.WriteLine("*HE SMILES AND DRAGS YOU TO THE DRINK TABLE*");
+                        Console.WriteLine("*HE FIXES HIMSELF A CUP AND DRINKS IT IN ONE GULP, HE THEN MAKES ANOTHER CUP AND OFFERS IT TO YOU*");
 
+                        Console.WriteLine("---------------------------------------------------------------------------");
+                        Console.WriteLine("Do you take the cup or decline?:");
+                        string choice14 = Console.ReadLine();
                     }
                     else//Go talk to Emmanuelle
                     {
@@ -51,7 +58,24 @@ namespace climaxTextAdventure
                 }
                 else // Talk to Lou
                 {
+                    Console.WriteLine("*YOU WALK OVER TO LOU WHO IS SITTING IN A CHAIR AGAINST THE WALL, SHE SEEMS DISTANT FROM THE PARTY GOING ON*");
+                    Console.WriteLine("YOU: What's up with you?");
+                    Console.WriteLine("LOU: Not feeling great.");
+                    Console.WriteLine("YOU: Want me to get you some punch?");
+                    Console.WriteLine("LOU: No, I think that would make me feel worse.");
 
+                    Console.WriteLine("---------------------------------------------------------------------------");
+                    Console.WriteLine("Do you try lift Lou's spirit or go hang out with David? (Type Lift Spirit or Hang With David");
+                    string choice13 = Console.ReadLine();
+
+                    if (choice13 == "Lift Spirit" || choice13 == "lift spirit")// lift Lou's spirit
+                    {
+
+                    }
+                    else //Go Hangout with David
+                    {
+                            
+                    }
                 }
             }
             else // Walk to Daddy
@@ -75,7 +99,7 @@ namespace climaxTextAdventure
                     Console.WriteLine("Do you give them some your personal stash of weed or not? (Type: Give or Lie)");
                     string Choice21 = Console.ReadLine();
 
-                    if (Choice21 == "Give")//Give Psyche and Ivana weed
+                    if (Choice21 == "Give" || Choice21 == "give")//Give Psyche and Ivana weed
                     {
                         Console.WriteLine("YOU: Here, but I expect atleast some money in return.");
                         Console.WriteLine("*YOU GIVE THEM SOME WEED OUT OF YOUR POCKET*");
@@ -88,7 +112,24 @@ namespace climaxTextAdventure
                 }
                 else // Talk to Emmanuelle and Daddy
                 {
+                    Console.WriteLine("*You walk up to Emmanuelle and hug her from behind*");
+                    Console.WriteLine("YOU: How was it really?");
+                    Console.WriteLine("EMMANUELLE: AMAZING! I really couldn't be more proud.");
+                    Console.WriteLine("YOU: I am honestly still so nervous.");
+                    Console.WriteLine("EMMANUELLE: You are doing amazing! Give yourself a break and let loose a little. Go have some punch!");
 
+                    Console.WriteLine("---------------------------------------------------------------------------");
+                    Console.WriteLine("Do you go get some punch or stay with Daddy and Emmanuelle? (Type Punch or Stay)");
+                    string choice22 = Console.ReadLine();
+
+                    if (choice22 == "Punch" || choice22 == "punch")
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
                 }
             }
         }
